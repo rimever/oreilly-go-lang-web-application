5567f4d5bce0db120fba4d570ed9bf8c03f62061
rimever <mot_siva_ordin@hotmail.com>
2021-06-26 15:14:53 +0900

    proceed 1-1-1-2

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-<html>
-	<head>
-		<title>Chat</title>
-	</head>
-	<body>
-		Let's Chat
-	</body>
-</html>
-`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	template *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		path, err := os.Getwd()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println(path)
+		// current directory is chapter1
+		t.template = template.Must(template.ParseFiles(filepath.Join("chat/templates", t.filename)))
 	})
-	if err := http.ListenAndServe(":8080",nil); err != nil {
-		log.Fatal("ListenAndServe:",err)
+	if err := t.template.Execute(w, nil); err != nil {
+		log.Fatal("Execute",err)
 	}
-	// http://localhost:8080/
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+	// Access http://localhost:8080/
 }
